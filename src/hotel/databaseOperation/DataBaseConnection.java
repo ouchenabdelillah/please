52a1d31f8e9878bebce2de95d103719cdc960c81
MerouaneBoucheriba <marouane.boucheriba@univ-constantine2.dz>
2025-04-25 23:44:32 +0100

    security

--- src/hotel/databaseOperation/DataBaseConnection.java
+++ src/hotel/databaseOperation/DataBaseConnection.java
@@ -34,20 +34,21 @@ class DataBaseConnectionTest {
 
 public class DataBaseConnection {
 
-	static String url= "jdbc:mysql://localhost:3306/hotel?useUnicode=true" + 
-			"&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&" + 
-			"serverTimezone=UTC" ;
-    public static Connection connectTODB(){
+    static String url = "jdbc:mysql://localhost:3306/hotel?useUnicode=true" +
+            "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&" +
+            "serverTimezone=UTC";
+
+    public static Connection connectTODB() {
+        String dbUser = System.getenv("DB_USER");
+        String dbPass = System.getenv("DB_PASS");
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            return  DriverManager.getConnection(url,"root","root");
+            return DriverManager.getConnection(url, dbUser, dbPass);
         } catch (Exception e) {
-        	
             System.err.println("Connection error");
             e.printStackTrace();
             return null;
         }
-      
     }
-    
 }
